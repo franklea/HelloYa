94340bce6ac8687bf4f9a6bb2e3c8a3019347ea5
leon <1>
2018-03-07 20:48:48 +0800

    map add

--- app/src/main/java/com/pwj/helloya/MainActivity.java
+++ app/src/main/java/com/pwj/helloya/MainActivity.java
@@ -6,13 +6,18 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
 
 public class MainActivity extends AppCompatActivity {
     private String[] mTitles = new String[]{"首页", "地址","电话", "设置"};
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
@@ -26,4 +31,5 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.getTabAt(3).setText(mTitles[3]).setIcon(R.mipmap.setting);
 
     }
+
 }
